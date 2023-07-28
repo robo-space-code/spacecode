22131b27f7ff05e38454257c64df424e22c7b3df
keyleeu <minhk.lee21@gmail.com>
2023-07-29 03:55:24 +0900

    32152356236

---
+++ src/main/resources/static/src/client/pixi/codeInterface/codeInterface.js
@@ -0,0 +1,21 @@
+export function playground() {
+
+    const canvasElement = document.getElementById('game-canvas');
+
+    const playgroundApp = new PIXI.Application({ 
+        view: canvasElement,
+        width: 1000, 
+        height: 1000,
+        antialias: true,
+        transparent: true
+    });
+    playgroundApp.view.style.position = "absolute";
+    playgroundApp.view.style.top = "50%";
+    playgroundApp.view.style.left = "5%";
+    playgroundApp.view.style.transform = "translateY(-50%)";
+    playgroundApp.view.style.border = "2px solid gray";
+
+    document.body.appendChild(playgroundApp.view);
+}
+
+export default playground;
