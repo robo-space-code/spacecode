c7a70db5736bba22f81235a93a87588c98957f9c
keyleeu <minhk.lee21@gmail.com>
2023-07-30 21:39:26 +0900

    34612623523

--- src/main/resources/static/src/client/codei.js
+++ src/main/resources/static/src/client/codei.js
@@ -1,43 +1,6 @@
-// Configure require.js
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs' }});
-
-// File objects
-var files = {
-  "player.java": {
-    name: "filename",
-    language: 'java',
-    value: ''
-  },
-  "given.java": {
-    name: "move.java",
-    language: "java",
-    value: ''
-  }
-};
-
-// Initial file
-var currentFile = files['player.java'];
-
-// Create Monaco Editor instance
-var editor = null;
-require(['vs/editor/editor.main'], function() {
-  editor = monaco.editor.create(document.getElementById('javaEditor'), {
-    value: currentFile.value,
-    language: currentFile.language
+export function createEditor(elementId) {
+  return CodeMirror.fromTextArea(document.getElementById(elementId), {
+      lineNumbers: true,  // Show line numbers
+      mode: 'javascript' // Use JavaScript syntax highlighting
   });
-});
-
-// Button click handlers
-document.getElementById('getValueButton').addEventListener('click', function() {
-  alert(editor.getValue());
-});
-document.getElementById('setFilePlayer').addEventListener('click', function() {
-  currentFile = files['player.java'];
-  editor.setValue(currentFile.value);
-  monaco.editor.setModelLanguage(editor.getModel(), currentFile.language);
-});
-document.getElementById('setFileGiven').addEventListener('click', function() {
-  currentFile = files['given.java'];
-  editor.setValue(currentFile.value);
-  monaco.editor.setModelLanguage(editor.getModel(), currentFile.language);
-});
+}
